e65dd1fef71682ba79767e04e5f8fc68b0030034
Raj <rajchevli999@gmail.com>
2019-08-11 07:51:50 +0530

    Backport dtb inclusion logic from Q
    In Q boot image with header version 2 supports inclusion of
    DTB image within the boot image.
    QCDT legacy devices work the same way except for the dtb
    size in bytes that is specified earlier in the final image.
    
    I backported Google's logic to P to get rid of qcom stuff in build/make.
    This is unneccessary on other roms at the moment but will be required in Q.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,12 +53,14 @@ ifneq ($(wildcard $(TOP_PATH)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-8.3),)
 endif
 
 # Kernel
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_MKBOOTIMG_ARGS += --qcdt
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
 
